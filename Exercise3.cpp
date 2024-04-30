03fbed671b2af81a630739c7f1a6136ba4a0cee0
dlskrud815 <dlskrud815@gmail.com>
2024-04-30 19:47:34 +0900

    index+1

--- Exercise3.cpp
+++ Exercise3.cpp
@@ -46,7 +46,7 @@ int main()
 	cin >> startIndex;
 	cout << "어디까지? ";
 	cin >> endIndex;
-	s.erase(startIndex, endIndex- startIndex);
+	s.erase(startIndex, endIndex- startIndex+1);
 	cout << "con이 되도록 변경.. " << s << endl;
 
 	return 0;

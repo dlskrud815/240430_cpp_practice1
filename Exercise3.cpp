1ed07f6e3ed1dcf9b6c4f497ecb859fb9bf908c1
dlskrud815 <dlskrud815@gmail.com>
2024-04-30 19:45:15 +0900

    입력부 추가

--- Exercise3.cpp
+++ Exercise3.cpp
@@ -30,7 +30,7 @@ int main()
 	cin >> endIndex;
 	cout << "어떤 문자로? ";
 	cin >> ch;
-	
+
 	for (int i = 0; i < s.length(); i++)
 	{
 		if (i >= startIndex && i <= endIndex)
@@ -41,12 +41,12 @@ int main()
 	cout << "coooooon이 되도록 변경.. " << s << endl << endl;
 
 	//5. "con"이 되도록 변경
-	cout << endl << "*변경할 인덱스 입력*" << endl
+	cout << endl << "*삭제할 인덱스 입력*" << endl
 		<< "어디서부터? ";
 	cin >> startIndex;
 	cout << "어디까지? ";
 	cin >> endIndex;
-	s.erase(startIndex, endIndex-startIndex);
+	s.erase(startIndex, endIndex);
 	cout << "con이 되도록 변경.. " << s << endl;
 
 	return 0;
